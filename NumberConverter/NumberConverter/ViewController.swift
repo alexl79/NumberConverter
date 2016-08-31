edb7366c2fa39cd4707ef64778c9d79f5aaea368
Alexander Lobanov <alex_l@inbox.ru>
2016-08-31 22:19:48 +0300

    project init framework init

---
+++ NumberConverter/NumberConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NumberConverter
+//
+//  Created by Alexander Lobanov on 31.08.16.
+//  Copyright © 2016 SurfStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NumberConverterFrm/NumberConverterFrm/NumberConverterFrm.h
@@ -0,0 +1,19 @@
+//
+//  NumberConverterFrm.h
+//  NumberConverterFrm
+//
+//  Created by Alexander Lobanov on 31.08.16.
+//  Copyright © 2016 SurfStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NumberConverterFrm.
+FOUNDATION_EXPORT double NumberConverterFrmVersionNumber;
+
+//! Project version string for NumberConverterFrm.
+FOUNDATION_EXPORT const unsigned char NumberConverterFrmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumberConverterFrm/PublicHeader.h>
+
+
